a5f14dd847478385ba0522cc1d1b4710114780d2
ChrisDeam53 <christopher.deam@ntlworld.com>
2022-12-16 16:09:00 +0000

    PROJ-Initial-Setup Fixed Morse->ASCII with working ASCII spaces v2

--- assignment/src/assignment/core.clj
+++ assignment/src/assignment/core.clj
@@ -34,6 +34,10 @@
 (require '[clojure.string :as str])
 (require '[clojure.set :as cljSet])
 
+;; /****************************************************************************************/
+;; ASCII->Morse & Morse->ASCII solution.
+;; /****************************************************************************************/
+
 (def letter-hash-map
   "Define strings to their respective Morse counterparts."
   {"A" ".-", "B" "-...", "C" "-.-.", "D" "-..", "E" ".", "F" "..-.", "G" "--.", "H" "....",
@@ -49,17 +53,17 @@
    Arguments: currentCharacter - Individual Character in the string to perform the get method."
   (let [returnString (get letter-hash-map currentCharacter)]
     (if (str/includes? returnString " ")
-      ;; Append 3x spaces to the end, totals to 7 spaces between words.
-      (str (str/trim (get letter-hash-map currentCharacter)) "   ")
+      ;; Append 5x spaces to the end, totals to 7x spaces between words.
+      (str (str/trim (get letter-hash-map currentCharacter)) "    ")
       ;; Append 2x spaces to the end of each morse character returned if not a space.
-      (str returnString "  "))))
+      (str returnString "   "))))
 
 (defn translate-character-and-space-to-ascii [currentString]
   "Performs string manipulation to split two characters separated by a space.
    Arguments: currentString - Individual String in the string to perform the get method."
-  ;; NOTE: Needs to return a string - java.lang.String
   (let [firstCharacter (first currentString)]
     (let [lastCharacter (last currentString)]
+      ;; Get the characters *before* and *after* the space & return with a singular space.
       (str (get morse-hash-map firstCharacter) " " (get morse-hash-map lastCharacter)))))
 
 (defn translate-character-to-ascii [currentCharacter]
@@ -68,31 +72,27 @@
   (if (str/includes? currentCharacter " ")
     ;; String contains morse spaces (7x spaces).
     (let [returnString (str/split currentCharacter #"       ")]
+      ;; As Regex includes the 7x spaces, string must be split to return properly.
       (translate-character-and-space-to-ascii returnString))
     ;; No spaces. return as normal.
     (get morse-hash-map currentCharacter)))
 
 (defn get-character [enteredString]
-  "TODO: This method.
+  "Gets the respective alternate character for the string inputted & returns these mapped values.
    Arguments: enteredString - The string to be converted to its appropriate counterpart."
     ;; Covert all characters to uppercase for uniformty.
   (let [characterVectorUpper (str/upper-case enteredString)]
     ;; Use Regex to get each individual character, including spaces. Returns a vector.
     (if (or (= (get characterVectorUpper 0) \.) (= (get characterVectorUpper 0) \-))
-      ;; (let [characterVector (str/split characterVectorUpper #"\s+")]
       (let [characterVector (str/split characterVectorUpper #"\s+(?!\s{3})(?<!\s{4})")]
         ;; String entered is Morse.
         ;; Join all empty spaces between. Example: "A B" -> "AB".
         (str/join "" (map translate-character-to-ascii characterVector)))
       (let [characterVector (str/split characterVectorUpper #"")]
         ;; String entered is ASCII.
-        (map translate-character-to-morse characterVector)))))
+        (apply str(map translate-character-to-morse characterVector))))))
 
-
-;; \s+(?!\s{3})(?<!\s{4})
-;; \s+(?!\s{3})(?<!\s{4})/g
-;; \(?<!) {3} (?!)
-
+;; EXAMPLE STRINGS:
 ;; Hello World
 ;; ....   .   .-..   .-..   ---       .--   ---   .-.   .-..   -..
 
@@ -103,8 +103,34 @@
   (let [enteredString (str (read-line))]
     (println(get-character enteredString))))
 
-(defn func2 []
-  (println "BBBBBBBBBBBB"))
+;; /****************************************************************************************/
+;; CET Solution.
+;; /****************************************************************************************/
+
+(defn slurp-1772-file []
+  "Slurps the 1772toDate.txt file line by line."
+  (println (slurp "src/assignment/1772toDate.txt")))
+
+(defn slurp-2019-file []
+  "Slurps the 2019.txt file line by line."
+  (println (slurp "src/assignment/2019.txt")))
+
+(defn slurp-2020-file []
+  "Slurps the 2020.txt file line by line."
+  (println (slurp "src/assignment/2020.txt")))
+
+(defn slurp-2021-file []
+  "Slurps the 2021.txt file line by line."
+  (println (slurp "src/assignment/2021.txt")))
+
+(defn slurp-2022-file []
+  "Slurps the 2022.txt file line by line."
+  (println (slurp "src/assignment/2022.txt")))
+
+(defn initialise-cet-solution []
+  "Initialise the solution. Slurp the respective text files."
+  (println "HIT!")
+  (slurp-1772-file))
 
 (defn initialise-project []
   "Initialise the project. Run solution depending on user input."
@@ -117,17 +143,15 @@
       (cond (= 1 userInput)
             (request-string-to-convert)
             (= 2 userInput)
-            (func2)
+            (initialise-cet-solution)
             :else
             (println "Unnacepted Request.")
             )))
         
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!")
-  (println "Executing project:\n Author: Chris Deam")
+(defn -main [& args]
+  "Entry point for the program."
+  (println "Executing project:\n Author: Chris Deam.")
   (initialise-project)
 )
 
